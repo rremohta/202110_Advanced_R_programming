233c75966510b9c09b996d47a6e33528f8dbf021
rremohta <roland@emohta.ch>
2021-10-14 10:19:09 +0200

    initial commit

---
+++ Start.R
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------------------------------
+# --- Zurich R Courses -- Advanced programming in R
+# --------------------------------------------------------------------------------------------------
+#
+# Benötigte R-Pakete
+library(devtools)
+library(revdepcheck)
+library(miniCRAN)
+library(rhub)
+library(Rhub)
+library(profvis)
+library(covr)
+library(microbenchmark)
+library(lattice)
+#
+#
+#
